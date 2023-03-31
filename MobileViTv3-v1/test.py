681a38bcc4d936101f15f27fa9b57892f2a30822
Евгений Бакаев <ebakaev@eclient24.ru>
2023-03-31 12:23:46 +0300

    fix paths

--- MobileViTv3-v1/test.py
+++ MobileViTv3-v1/test.py
@@ -1,5 +1,11 @@
 import torch
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 temp_model = torch.load('model_structure.pt')
+
+temp_model.load_state_dict(torch.load(
+    '../models/MobileViTv3-v1/results_classification/mobilevitv3_S_e300_7930/checkpoint_ema_best.pt',
+    map_location=device))
 
 print(temp_model)
